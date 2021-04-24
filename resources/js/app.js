665f3a8ee742ee205b4fd5af7cda453f82827778
remihayrimbault <remi.hayrimbault@gmail.com>
2021-04-24 12:07:41 +0200

    Mise à jour du app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,48 @@
-require('./bootstrap');
+$(document).ready(function() {
+    $(".transition").css("height", "0");
+});
+
+$('#search_bar').keyup(function (e) {
+    if (e.keyCode === 13) {
+        $(".transition").css("height", "100vh");
+        let val = $(this).val();
+        val = val.replace(/\s/g, "-");
+        setTimeout(function(){ window.top.location.href= `/research/${val}`; }, 3000);
+    }
+});
+
+$(".result > img").hover(function(){
+    $id = $(this).attr("id");
+    $(".background > img.research").attr('src',$id);
+    $(".background > img.research").css("opacity", "1");
+}, function(){
+    $(".background > img.research").css("opacity", "0");
+});
+
+$('div.result').click(function(){
+    $(".transition").css("height", "100vh");
+    let val = $(this).attr('id');
+    setTimeout(function(){ window.top.location.href= `/result/${val}`; }, 3000);
+});
+
+$('.back_button').click(function(){
+    $(".transition").css("height", "100vh");
+    setTimeout(function(){ history.back();}, 3000);
+});
+
+$('.try').click(function(){
+    $(".transition").css("height", "100vh");
+    setTimeout(function(){ history.back();}, 3000);
+});
+
+$("#search_bar").focus(function(){
+    $("#search_bar").css("transition-duration", "1s");
+    $("#search_bar").css("background", "#3f51b5");
+    $("#search_bar").css("color", "white");
+});
+
+$("#search_bar").focusout(function(){
+    $("#search_bar").css("transition-duration", "1s");
+    $("#search_bar").css("background", "none");
+    $("#search_bar").css("color", "#3f51b5");
+});
